4885ba6e801a01f03dc845c17f053d2cf55c5835
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2025-02-04 14:28:41 +0530

    feat: setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import { useState } from 'react'
-import './App.css'
-import Home from '../pages/Home'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Chat from "../pages/Chat";
 function App() {
-
   return (
-
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-   
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/chats" element={<Chat />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
